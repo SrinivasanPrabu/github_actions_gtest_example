65e90713ca2a2b5a01eda52abe9226684e74e873
frg1cob <gurumalleshprabu.srinivasan@in.bosch.com>
2023-07-27 15:00:28 +0530

    Typetest

---
+++ test/typeParameterizationExample.cpp
@@ -0,0 +1,50 @@
+#include "gtest/gtest.h"
+using testing::Types;
+
+class ITempSensor{
+  public:
+  virtual int getOutSideTemp()=0;
+};
+class ModelATempSensor:public ITempSensor{
+  public:
+  virtual int getOutSideTemp(){return 23;}
+};
+class ModelBTempSesnor:public ITempSensor{
+  public:
+  virtual int getOutSideTemp(){return 23;}
+};
+class AutoTempRegulator{
+  ITempSensor*tempSensorPtr;
+  public:
+  AutoTempRegulator(ITempSensor*address):tempSensorPtr{address} {}
+    void regulateTemp(){
+         return this->tempSesnorPtr->getOutsideTemp();
+    }
+};
+
+template <typename T>
+ITempSensor*createObject();
+template <>
+ITempSensor* createObject<ModelATempSensor>() { return new ModelATempSensor(); }
+template <>
+ITempSensor* createObject<ModelBTempSesnor>() { return new ModelBTempSesnor(); }
+
+template <typename T>
+class ModelATempSensorFixture:public testing::Test{
+  protected:
+  ModelATempSensorFixture():objUnder
+  //arrange
+  ITempSensor* objUndertest;
+
+}
+//TEST_F(ModelATempSensorFixture,GetTempTest){
+//  ASSERT_EQ(objUnderTest.getOutsideTemp(),23);
+//}
+
+typedef Types.<ModelATempSensor,ModelBTempSesnor> Implementation;
+
+TYPED_TEST_SUITE(TempSensorFixture,Implementations);
+
+TYPED_TEST(TempSensorFixture,getTempTest){
+ASSERT_EQ(objUnderTest->getOutsideTemp(),23);
+};
