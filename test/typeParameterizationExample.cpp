e73f5836e6e2fafd84f520f08e296e9e5ad101a4
frg1cob <gurumalleshprabu.srinivasan@in.bosch.com>
2023-07-27 16:55:56 +0530

    expect

--- test/typeParameterizationExample.cpp
+++ test/typeParameterizationExample.cpp
@@ -1,56 +1,322 @@
 #include "gtest/gtest.h"
+
+
+
+
 using testing::Types;
 
+
+
+
+ 
+
+
+
+
 class ITempSensor{
-  public:
-  virtual int getOutSideTemp()=0;
-};
-class ModelATempSensor:public ITempSensor{
-  public:
-  virtual int getOutSideTemp(){return 23;}
-};
-class ModelBTempSesnor:public ITempSensor{
-  public:
-  virtual int getOutSideTemp(){return 23;}
-};
-class ModelCTempSensor: public ITempSensor{
-  public:
-  virtual int getOutsidetemp(){return 23;}
-}:
-class AutoTempRegulator{
-  ITempSensor*tempSensorPtr;
-  public:
-  AutoTempRegulator(ITempSensor*address):tempSensorPtr{address} {}
-    void regulateTemp(){
-         return this->tempSesnorPtr->getOutsideTemp();
-    }
+
+
+
+
+    public:
+
+
+
+
+    virtual int getOutSideTemp()=0;
+
+
+
+
 };
 
+
+
+
+class ModelATempSensor:public ITempSensor{
+
+
+
+
+    public:
+
+
+
+
+    int getOutSideTemp(){
+
+
+
+
+        return 23;
+
+
+
+
+    }
+
+
+
+
+};
+
+
+
+
+class ModelBTempSensor:public ITempSensor{
+
+
+
+
+    public:
+
+
+
+
+    int getOutSideTemp(){
+
+
+
+
+        return 23;
+
+
+
+
+    }
+
+
+
+
+};
+
+
+
+
+class ModelCTempSensor:public ITempSensor{
+
+
+
+
+    public:
+
+
+
+
+    int getOutSideTemp(){
+
+
+
+
+        return 23;
+
+
+
+
+    }
+
+
+
+
+};
+
+
+
+
+class AutoTempRegulator{
+
+
+
+
+    //DIP: Abstraction
+
+
+
+
+    ITempSensor* tempSensorPtr;
+
+
+
+
+    public:
+
+
+
+
+    AutoTempRegulator(ITempSensor* address):tempSensorPtr(address){}
+
+
+
+
+    int regulateTemp(){
+
+
+
+
+        return this->tempSensorPtr->getOutSideTemp();
+
+
+
+
+    }
+
+
+
+
+};
+
+
+
+
+ 
+
+
+
+
 template <typename T>
-ITempSensor*createObject();
+
+
+
+
+ITempSensor* createObject();
+
+
+
+
 template <>
+
+
+
+
 ITempSensor* createObject<ModelATempSensor>() { return new ModelATempSensor(); }
+
+
+
+
 template <>
-ITempSensor* createObject<ModelBTempSesnor>() { return new ModelBTempSesnor(); }
+
+
+
+
+ITempSensor* createObject<ModelBTempSensor>() { return new ModelBTempSensor(); }
+
+
+
+
 template <>
+
+
+
+
 ITempSensor* createObject<ModelCTempSensor>() { return new ModelCTempSensor(); }
 
+
+
+
+ 
+
+
+
+
 template <typename T>
-class ModelATempSensorFixture:public testing::Test{
-  protected:
-  ModelATempSensorFixture():objUnder
-  //arrange
-  ITempSensor* objUndertest;
+
+
+
+
+class TempSensorFixture:public testing::Test{
+
+
+
+
+    protected:
+
+
+
+
+    //Arrange
+
+
+
+
+    ITempSensor* objUnderTest;
+
+
+
+
+    TempSensorFixture() :objUnderTest{ createObject<T>() } {}
+
+
+
+
+};
+
+
+
+
+ 
+
+
+
+
+typedef Types<ModelATempSensor,ModelBTempSensor,ModelCTempSensor> Implementations;
+
+
+
+
+ 
+
+
+
+
+TYPED_TEST_SUITE(TempSensorFixture, Implementations);
+
+
+
+
+ 
+
+
+
+
+TYPED_TEST(TempSensorFixture, GetTempTest) {
+
+
+
+
+    ASSERT_EQ(this->objUnderTest->getOutSideTemp(),23);
+
+
+
 
 }
-//TEST_F(ModelATempSensorFixture,GetTempTest){
-//  ASSERT_EQ(objUnderTest.getOutsideTemp(),23);
-//}
 
-typedef Types.<ModelATempSensor,ModelBTempSesnor> Implementation;
 
-TYPED_TEST_SUITE(TempSensorFixture,Implementations);
 
-TYPED_TEST(TempSensorFixture,getTempTest){
-ASSERT_EQ(objUnderTest->getOutsideTemp(),23);
+
+ class FakeTempSensor:public ITempSensor{
+
+    public:
+
+    int getOutsideTempCallCount=0;
+
+    int getOutSideTemp(){ getOutsideTempCallCount+=1;return  0;}
+
 };
+
+
+
+
+
+
+
+TEST(AutoTempRegulatorTestSuite,RegulateTempTest){
+
+    FakeTempSensor mockObj;
+
+    AutoTempRegulator codeUnderTest(&mockObj);
+
+    codeUnderTest.regulateTemp(); //Interaction
+
+    ASSERT_EQ(mockObj.getOutsideTempCallCount, 1); //Expectation
+
+}
